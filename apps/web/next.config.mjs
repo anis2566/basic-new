b432d3600f705cfa779b23ff141f1d7d5d215f37
anis2566 <anichuranis1000@gmail.com>
2025-09-02 17:36:52 +0600

    faa

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,19 +1,6 @@
-// C:\Users\Basic\Documents\monorepo\apps\web\next.config.mjs
-
-// Remove this line: const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
-// And use this line instead:
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(new PrismaPlugin());
-    }
-    return config;
-  },
+  transpilePackages: ["@workspace/ui"],
 };
 
-// Remove this line: module.exports = nextConfig;
-// And use this line instead:
 export default nextConfig;
