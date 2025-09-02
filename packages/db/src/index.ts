cc4f7d30514ee76c69ba50011e4f774adbb04a13
anis2566 <anichuranis1000@gmail.com>
2025-09-02 15:36:54 +0600

    deploy

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,25 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
 import { PrismaClient } from "./generated/prisma";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
-export const prisma =
-    globalForPrisma.prisma || new PrismaClient();
+if (!process.env.DATABASE_URL) {
+    dotenv.config({ path: path.join(__dirname, '../../../.env') });
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (!process.env.DATABASE_URL) {
+    dotenv.config({ path: path.join(process.cwd(), '../../.env') });
+}
 
-export * from "./generated/prisma";
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
