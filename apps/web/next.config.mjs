9fe9f86547b18563605daa339c83cc2f82970bcc
anis2566 <anichuranis1000@gmail.com>
2025-09-02 16:53:02 +0600

    faa

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
+};
 
-export default nextConfig
+export default nextConfig;
